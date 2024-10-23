a2402710bd3dfa9def70ff4b3fc8a4929597bf78
anishiocod <anish@iocod.com>
2024-10-23 12:17:27 +0530

    Changes recept incomming as binary

--- src/main/java/com/iocod/spring_mongo_test/controller/ReceiptController.java
+++ src/main/java/com/iocod/spring_mongo_test/controller/ReceiptController.java
@@ -9,44 +9,62 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-// import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/receipts")
 public class ReceiptController {
+
     private static final Logger logger = LoggerFactory.getLogger(ReceiptController.class);
 
     @Autowired
     private ReceiptService receiptService;
 
     @PostMapping
-    public ResponseEntity<Receipt> createReceipt(@RequestBody Receipt receipt) {
+    public ResponseEntity<Receipt> createReceipt(@RequestBody byte[] binaryData) {
         long startTime = System.currentTimeMillis();
-        logger.info("Received request to create receipt: {}", receipt);
-        logger.info("Receipt Name",receipt.getName());
+        logger.info("Received request with binary data");
 
         try {
+            // Parse binary data into key-value pairs
+            String dataString = new String(binaryData, StandardCharsets.UTF_8);
+            Map<String, String> parameters = parseWebhookData(dataString);
+
+            // Create a new Receipt object and populate it from parsed data
+            Receipt receipt = new Receipt();
+            receipt.setApiVersion(parameters.get("ApiVersion"));
+            receipt.setMessageStatus(parameters.get("MessageStatus"));
+            receipt.setSmsSid(parameters.get("SmsSid"));
+            receipt.setTo(parameters.get("To"));
+            receipt.setFrom(parameters.get("From"));
+            receipt.setAccountSid(parameters.get("AccountSid"));
+
+            // Save the receipt using the service
             Receipt savedReceipt = receiptService.saveReceipt(receipt);
             long endTime = System.currentTimeMillis();
             long duration = endTime - startTime;
 
-            logger.info("Successfully created receipt. Time taken: {} ms", duration);
+            logger.info("Successfully created receipt with data: {}", receipt);
+            logger.info("Time taken: {} ms", duration);
             return new ResponseEntity<>(savedReceipt, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e) {
-            // Log the error and return a 400 Bad Request response
-            logger.error("Invalid request body: {}", e.getMessage());
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
         } catch (Exception e) {
-            // Log the error and return a 500 Internal Server Error response
-            logger.error("Failed to create receipt. Reason: {}", e.getMessage());
-            e.printStackTrace();
+            logger.error("Failed to process receipt data: {}", e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGeneralException(Exception e) {
-        logger.error("Unexpected error occurred during request processing", e);
-        return new ResponseEntity<>("Internal server error occurred. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+    private Map<String, String> parseWebhookData(String dataString) {
+        Map<String, String> parameters = new HashMap<>();
+        String[] pairs = dataString.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                parameters.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return parameters;
     }
 }
--- src/main/java/com/iocod/spring_mongo_test/model/Receipt.java
+++ src/main/java/com/iocod/spring_mongo_test/model/Receipt.java
@@ -7,23 +7,60 @@ import lombok.Data;
 @Document(collection = "receipts")
 public class Receipt {
     @Id
-    private String id;
-    private String name;
-    private String email;
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
+    private String apiVersion;
+    private String messageStatus;
+    private String smsSid;
+    private String to;
+    private String from;
+    private String accountSid;
+     // Getter methods
+     public String getApiVersion() {
+        return apiVersion;
     }
 
-    public void setEmail(String email) {
-        this.email = email; 
+    public String getMessageStatus() {
+        return messageStatus;
+    }
 
+    public String getSmsSid() {
+        return smsSid;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    // Setter methods
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    public void setMessageStatus(String messageStatus) {
+        this.messageStatus = messageStatus;
+    }
+
+    public void setSmsSid(String smsSid) {
+        this.smsSid = smsSid;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setAccountSid(String accountSid) {
+        this.accountSid = accountSid;
     }
 
 }
